5ace506642e1fe3271ef77b340e935fde076e055
OmkarDhanke <dhankeomkar5@gmail.com>
2025-12-17 22:42:28 +0530

    Fix formatting of RegionCode column in EmployeePerformance table

--- SQL-Expedition/Practice/P Day 5-6.sql
+++ SQL-Expedition/Practice/P Day 5-6.sql
@@ -4,7 +4,7 @@ CREATE TABLE EmployeePerformance (
     EmployeeID INT PRIMARY KEY,
     FullName VARCHAR(100),
     Department VARCHAR(50),
-    RegionCode VARCHAR(5),
+    RegionCode VARCHAR(5), 
     PerformanceScore INT,
     ProjectsCompleted INT
 );
